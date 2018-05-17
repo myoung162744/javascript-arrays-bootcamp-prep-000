d4d0c6ea239bc7988eb827942466438b8c8cb40d
Matthew Young <myoung162744@gmail.com>
2018-05-17 15:59:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -49,3 +49,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
+}
