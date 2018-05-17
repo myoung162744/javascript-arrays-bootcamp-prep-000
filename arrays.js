3504f984e1277359c6d74e90c2411b6cdf86018b
Matthew Young <myoung162744@gmail.com>
2018-05-17 16:10:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,33 +28,41 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  array = array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
-  [...array, element]
+  array = [...array, element]
+  return array
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
+  array = array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index) {
-  array[index]
+  array = array[index]
+  return array
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift()
+  array = array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
+  array = array.slice(1)
+  return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
+  array = array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
+  array = array.slice(0, array.length - 1)
+  return array
 }
