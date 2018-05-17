3d10d8c34a38b9a9b44c0bf0789884199a15e68b
Matthew Young <myoung162744@gmail.com>
2018-05-17 15:25:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+]
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray(array, string) {
+  [string, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, string) {
+  array.unshift(string)
+}
+
+function addElementToEndOfArray(array, string) {
+  [...array, string]
+}
+
+function destructivelyAddElementToEndOfArray(array, string) {
+  array.push(string)
+}
